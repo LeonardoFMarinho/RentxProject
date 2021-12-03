b0f69abca06ddea6c2c9f3b7c91f175af20c19ab
Leonardo <leonardo.fmarinho.dev@hotmail.com>
2021-12-03 10:19:31 -0300

    Create User Admin

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -1,5 +1,4 @@
 import { hash } from 'bcrypt';
-import { getConnection } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
 import createConnection from '../index';
@@ -7,10 +6,10 @@ import createConnection from '../index';
 async function create() {
   const connection = await createConnection('localhost');
 
-  const id = uuidv4;
+  const id = uuidv4();
   const password = await hash('admin', 8);
   await connection.query(
-    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_licecnse)
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
       values('${id}','admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
    `,
   );
